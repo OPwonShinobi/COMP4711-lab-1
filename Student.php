0c2db3b8ff0ca98d1dba1fddd126a25aad53ea08
Alex Xia <xiaalex1998@gmail.com>
2018-01-10 23:44:57 -0800

    StudentFixed a bug, if student has no grade, will get DivideByZeroException. Added comments to index.php and Student.php

--- Student.php
+++ Student.php
@@ -1,48 +1,73 @@
 <?php
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * COMP4711 lab 1
  */
 
 /**
- * Description of Student
+ * Basic Student class. Allows setting emails and grades for each student.
  *
- * @author Alex
+ * @author Alex Xia
  */
 class Student {
-    //put your code here
+    /**
+     * Constructor.
+     */
     function __construct() {
         $this->surname = '';
         $this->first_name = '';
         $this->emails = array();
         $this->grades = array();
     }
-    
+    /**
+     * Adds an email at a key $which.
+     * Allows overwriting emails by using the same
+     * key twice.
+     * @param type $which index to add an email
+     * @param type $address the email string
+     */
     function add_email($which,$address) {
         $this->emails[$which] = $address;
     }
 
+    /**
+     * Adds a grade to the student instance.
+     * Because grades array auto-increment, once added
+     * cannot be changed or removed.
+     * @param type $grade
+     */
     function add_grade($grade) {
         $this->grades[] = $grade;
     }
     
+    /**
+     * Gets the average grade of the student instance.
+     * If student has no grades, returns 0;
+     * @return double average grade
+     */
     function average() {
         $total = 0;
         foreach($this->grades as $value) {
             $total += $value;
         }
+        if (count($this->grades) == 0) {
+            return 0;
+        }
         return $total / count($this->grades);
     }
     
+    /**
+     * Gets a student's first and last name, average grade,
+     * and email as a pre-formated html tag.
+     * @return string pre tag
+     */
     function to_string() {
         $result = $this->first_name . " " . $this->surname;
         $result .= " (" . $this->average() . ")\n";
         foreach ($this->emails as $index=>$email) {
-            $result .= $index . ": " . $email;
+            $result .= $index . ": " . $email . "\n";
         }
-        $result .= "\n";
+//        $result .= "\n";
         return '<pre>'.$result.'</pre>';
     }
 }
